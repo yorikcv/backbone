9fce1fee8bf7bddb981ec8eee5f67f07f26514f7
yorikcv <yorik.cv@gmail.com>
2014-08-12 13:04:56 +0300

    last update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
 ﻿$(function () {
 
+	var Tweet = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				author: 'def',
+				status: ''
+			}
+		}
+
+	});
+
+	var TweetList = Backbone.Collection.extend({
+		model: Tweet
+	});
+
+	var tweetList = new TweetList();
+
+	$("#button_add").click(function() {
+		var tweet = new Tweet({author: $("#author_name").val(), status: $("#status_name").val()});
+
+		tweetList.add(tweet);
+		console.log(tweetList.toJSON());
+	});
+
+	
+    
+
+    
+
     
 });
     
